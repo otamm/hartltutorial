e04a4632aba1a912805affc155f6c3e0b1b967a8
Otavio <otamm@hotmail.com>
2014-11-17 11:15:34 -0200

    Added index to email, changed 'name' to 'username'

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessor :name, :email
-  before_save { self.email = email.downcase , self.name = name.downcase}
+#  attr_accessor :name, :email
+  before_save { self.email = email.downcase , self.username = username.downcase}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates(:name,presence: true , length: { maximum: 19 }, uniqueness: true)
+  validates(:username,presence: true , length: { maximum: 19 }, uniqueness: true)
   validates(:email,presence: true,format: { with: VALID_EMAIL_REGEX }, uniqueness: true)
 end
--- db/migrate/20141003050135_create_users.rb
+++ db/migrate/20141003050135_create_users.rb
@@ -1,12 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :username
       t.string :email
 
-      t.timestamps
+      t.timestamps null: false
     end
-    add_index :users, :email, unique: true
-    add_index :users, :name, unique: true
   end
 end
